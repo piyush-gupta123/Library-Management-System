81989d93e5f8f6b97084cf022a6ce8812e8ba517
piyush-gupta123 <95485177+piyush-gupta123@users.noreply.github.com>
2023-06-06 19:55:25 +0530

    Work In Progress

---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGO_URI:
+        "mongodb+srv://PiyushGupta:Piyush12345@cluster0.4ku4jbe.mongodb.net/FirstDB?retryWrites=true&w=majority",
+    JWT_SECRET: "MOHENMONDALJDFKFLL",
+};
